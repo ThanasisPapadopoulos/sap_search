7044c918da7d2b4a3ec40b25df9f0e8c96cb1abb
ThanasisPapadopoulos <56246749+ThanasisPapadopoulos@users.noreply.github.com>
2023-05-16 11:51:32 +0300

    Waves with search bar focus updated

--- code/script.js
+++ code/script.js
@@ -1,15 +1,22 @@
 function search() {
-    let sentence = document.getElementById("sentence").value;
-    let query = "sap abap " + sentence;
-    let url = "https://www.google.com/search?q=" + encodeURIComponent(query);
-    window.open(url, "_self");
+  let sentence = document.getElementById("sentence").value;
+  let query = "sap abap " + sentence;
+  let url = "https://www.google.com/search?q=" + encodeURIComponent(query);
+  window.open(url, "_self");
+}
+
+let inputBox = document.getElementById("sentence");
+inputBox.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    search();
   }
+});
 
-  let inputBox = document.getElementById("sentence");
-  inputBox.addEventListener("keydown", function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      search();
-    }
-  });
+window.onload = function () {
+  var searchInput = document.getElementById("sentence");
 
+  searchInput.focus();
+  searchInput.classList.add("animate");
+  btn.classList.add("animate");
+};
